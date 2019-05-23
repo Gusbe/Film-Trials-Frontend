4307276c1ff25506a5be3f3a2522040c26e8075c
gusbe <gusbertolo@gmail.com>
2019-05-23 21:53:11 +0200

    Created search route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom'
 import PrivateRoute from './components/PrivateRoute';
 import AnonRoute from './components/AnonRoute';
+import AuthProvider from './providers/AuthProvider';
 import Navbar from './components/Navbar';
-import Private from './pages/Private';
+// import Private from './pages/Private';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import AddLocation from './pages/AddLocation';
 import ViewLocation from './pages/ViewLocation';
-import AuthProvider from './providers/AuthProvider';
+import Search from './pages/Search';
+
 
 
 class App extends Component {
@@ -22,6 +24,7 @@ class App extends Component {
             <AnonRoute path="/login" component={Login} />
             <PrivateRoute path="/location/add" component={AddLocation} exact/>
             <Route path="/location/:id" component={ViewLocation}  />
+            <Route path="/search/:id" component={Search}  />
             {/* <PrivateRoute path="/private" component={Private} /> */}
           </Switch>
         </div>
---
+++ src/pages/Search.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import locationService from './../services/locationService';
+
+class Search extends Component {
+
+  constructor(props){
+    
+    super(props);
+
+    this.state = {
+
+    }
+
+  }
+
+  componentDidMount(){
+    
+
+  }
+
+  render(){    
+    return (
+      <div>
+        I'm a searcher!
+      </div>
+    );
+  }
+
+
+}
+
+
+export default Search;
