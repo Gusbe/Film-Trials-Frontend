1e4e332bd63cb9caa9f64fd2e03d1d84da16305e
gusbe <gusbertolo@gmail.com>
2019-05-26 23:04:27 +0200

    Styles and control errors in add location

--- src/pages/AddLocation.js
+++ src/pages/AddLocation.js
@@ -20,14 +20,15 @@ class AddLocation extends Component {
         zoom: 10,
         bearing: 0,
         pitch: 0,
-        width: 500,
-        height: 500,
+        width: '100vp',
+        height: 200,
       },
       currentPosition: {
         latitude: null,
         longitude: null
       },
-      savePosition: null
+      savePosition: null,
+      disable: true
     }
   }
 
@@ -60,21 +61,14 @@ class AddLocation extends Component {
     const savePosition = {
       lon: ev.lngLat[0],
       lat: ev.lngLat[1]
-    } 
+    }
     this.setState({savePosition, lat:savePosition.lat, lon:savePosition.lon});
   };
 
-
-
-
   handleChange = (event) => {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value});
   }
-
-
-
-
 
   fileOnchange = (event) => {
     const file = event.target.files[0];
@@ -83,18 +77,10 @@ class AddLocation extends Component {
 
     locationService.imageUpload(uploadData)
       .then((scenePictureUrl) => {
-        this.setState({
-          scenePictureUrl,
-          disable: false,
-        })
+        this.setState({scenePictureUrl})
       })
       .catch((error) => console.log(error))
   }
-
-
-
-
-
 
   handleFormSubmit = (event) => {
     event.preventDefault();
@@ -114,70 +100,96 @@ class AddLocation extends Component {
       });
   }
 
+  controlButton = () => {
+    if (
+      this.state.title
+      && this.state.scenePictureUrl
+      && this.state.lat
+      && this.state.lon
+    ){
+      return false
+    }
+    else{
+      return true;
+    }
+  }
+
+  componentDidUpdate() {
+    console.log("CHECKING...");
+    if(!this.controlButton() && this.state.disable){
+      console.log('Canvio a enable!');
+      this.setState({disable: false});
+    }
+    else if(this.controlButton() && !this.state.disable){
+      console.log('Canvio a disable!');
+      this.setState({disable: true});
+    }
+  }
+
   render() {
     const { viewport } = this.state;
-
+    
     return (
-      <>
-        <button onClick={this.locateUser}>CURRENT LOCATION</button>
+      <div className='add-location-page'>
+        <h2>Add a new movie location</h2>
+        <div className='add-location'>
+          <h3>1. Select location:</h3>
+          <button className='get-location-button' onClick={this.locateUser}>Current location</button>
+        </div>
+        <div className="add-map">
+          <ReactMapGL
+            {...viewport}
+            mapboxApiAccessToken={process.env.REACT_APP_MAP_TOKEN}
+            mapStyle='mapbox://styles/gusbe/cjw3cw74r0vw01cpiazy0w3f4'
+            onViewportChange={this.changeView}
+            onClick={this.handleClick}
+          >
 
-        <ReactMapGL
-          {...viewport}
-          mapboxApiAccessToken={process.env.REACT_APP_MAP_TOKEN}
-          mapStyle='mapbox://styles/gusbe/cjw3cw74r0vw01cpiazy0w3f4'
-          onViewportChange={this.changeView}
-          onClick={this.handleClick}
-        >
+            {this.state.currentPosition.latitude ? (
+              <Marker
+                key='currentLocation'
+                latitude={this.state.currentPosition.latitude}
+                longitude={this.state.currentPosition.longitude}
+                offsetLeft={-10}
+                offsetTop={-10}
+              >
+                <div>
+                  <img src='./../img/blue.svg' alt='savePosition' style={{ width: '20px' }} />
+                </div>
+              </Marker>
+            ) : null}
 
-          {this.state.currentPosition.latitude ? (
-            <Marker
-              key='currentLocation'
-              latitude={this.state.currentPosition.latitude}
-              longitude={this.state.currentPosition.longitude}
-              offsetLeft={-10}
-              offsetTop={-10}
-            >
-              <div>
-                <img src='./../img/blue.svg' alt='savePosition' style={{ width: '20px' }} />
-              </div>
-            </Marker>
-          ) : null}
-
-          {this.state.savePosition ? (
-            <Marker
-              key='saveLocation'
-              latitude={this.state.savePosition.lat}
-              longitude={this.state.savePosition.lon}
-              offsetLeft={-8}
-              offsetTop={-27}
-            >
-            <div>
-            <img src='./../img/pin.png' alt='savePosition' style={{ width: '16px' } } />
-            </div>
-            </Marker>
-          ) : null}
-
-
-        </ReactMapGL>
-
-
-
-        <form onSubmit={this.handleFormSubmit}>
-          <label name="title">Title:</label>
-          <input type="text" name="title" value={this.state.title} onChange={this.handleChange} />
-          <label name="lon">Lon:</label>
-          <input type="text" name="lon" value={this.state.lon} onChange={this.handleChange} />
-          <label name="lat">Lat:</label>
-          <input type="text" name="lat" value={this.state.lat} onChange={this.handleChange} />
+            {this.state.savePosition ? (
+              <Marker
+                key='saveLocation'
+                latitude={this.state.savePosition.lat}
+                longitude={this.state.savePosition.lon}
+                offsetLeft={-8}
+                offsetTop={-27}
+              >
+                <div>
+                  <img src='./../img/pin.png' alt='savePosition' style={{ width: '16px' }} />
+                </div>
+              </Marker>
+            ) : null}
 
 
-          <label name="image">Image</label>
-          <input type="file" onChange={this.fileOnchange} name="image" />
-
-          {this.state.disable ? <input type="submit" disabled></input> : <input type="submit"></input>}
-
-        </form >
-      </>
+          </ReactMapGL>
+        </div>
+        <div className="add-form">
+          <form onSubmit={this.handleFormSubmit}>
+            <label className='lat-lon-inputs' name="lon">Lon:</label>
+            <input className='lat-lon-inputs' type="text" name="lon" value={this.state.lon} onChange={this.handleChange} />
+            <label className='lat-lon-inputs' name="lat">Lat:</label>
+            <input className='lat-lon-inputs' type="text" name="lat" value={this.state.lat} onChange={this.handleChange} />
+            <h3>2. Movie title:</h3>
+            <input type="text" name="title" value={this.state.title} onChange={this.handleChange} />
+            <h3>3. Upload a scene picture:</h3>
+            <input type="file" onChange={this.fileOnchange} name="image" />
+            {this.state.disable ? <input id='add-button-disabled' type="submit" value='Complete all the fields' disabled></input> : <input id='add-button' type="submit"  value='Send new location'></input>}
+          </form>
+        </div>
+      </div>
     );
   }
 }
