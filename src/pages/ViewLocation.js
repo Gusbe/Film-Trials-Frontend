7ebed1752fcf787aed45f13fd6f7de6987d93134
gusbe <gusbertolo@gmail.com>
2019-05-27 01:37:53 +0200

    Styles in view locations

--- src/pages/ViewLocation.js
+++ src/pages/ViewLocation.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import locationService from '../lib/locationService';
 import {withAuth} from '../providers/AuthProvider';
+import ReactMapGL, { Marker } from 'react-map-gl';
 
 class ViewLocation extends Component {
 
@@ -16,21 +17,43 @@ class ViewLocation extends Component {
       lon: '',
       scenePictureUrl: '',
       author: '',
-      owner: false
+      owner: false,
+      viewport: {
+        latitude: null,
+        longitude: null,
+        zoom: null,
+        bearing: null,
+        pitch: null,
+        width: null,
+        height: null
+      },
+      currentPosition: {
+        latitude: null,
+        longitude: null
+      }
     }
   }
 
-  componentDidMount(){
-    
+  componentDidMount() {
     const { id } = this.props.match.params;
     locationService.view(id)
     .then((location) => {
       
       const { _id, scenePictureUrl, title, coords, user } = location;
 
-      let ownerState;
+      let ownerState = false;
       if(this.props.user._id === user._id){
         ownerState = true;  
+      }
+
+      let viewport = {
+        latitude: coords.coordinates[1],
+        longitude: coords.coordinates[0],
+        zoom: 14,
+        bearing: 0,
+        pitch: 0,
+        width: '100vp',
+        height: 150
       }
 
       this.setState({
@@ -40,26 +63,71 @@ class ViewLocation extends Component {
         lon: coords.coordinates[0],
         scenePictureUrl,
         author: user.username,
-        owner: ownerState
+        owner: ownerState,
+        viewport
       })
     })
   }
 
-  render(){    
+
+  render() {
+    const { viewport, lat, lon } = this.state;
     return (
-      <>
-        <h2>{this.state.title}</h2>
-        <img src={this.state.scenePictureUrl} alt={this.state.title}/>
-        <p>Lon: {this.state.lon}</p>
-        <p>Lat: {this.state.lat}</p>
-        <p>Author: {this.state.author}</p>
-        {this.state.owner ? <Link to={'/location/' + this.state.id + '/update'}>Update</Link> : null}
-        {this.state.owner ? <Link to={'/location/' + this.state.id + '/delete'}>Delete</Link> : null}
-      </>
+      <div className='view-page'>
+        <div className="view-map">
+          {lat ? (
+            <ReactMapGL
+              {...viewport}
+              mapboxApiAccessToken={process.env.REACT_APP_MAP_TOKEN}
+              mapStyle='mapbox://styles/gusbe/cjw3cw74r0vw01cpiazy0w3f4'
+            >
+
+              <Marker
+                key='sss'
+                latitude={lat}
+                longitude={lon}
+                offsetLeft={-10}
+                offsetTop={-10}
+              >
+                <div>
+                  <img src='/img/pin.png' alt='currentPosition' style={{ width: '20px' }} />
+                </div>
+              </Marker>
+
+              {this.state.currentPosition.latitude ? (
+                <Marker
+                  key='currentLocation'
+                  latitude={this.state.currentPosition.latitude}
+                  longitude={this.state.currentPosition.longitude}
+                  offsetLeft={-10}
+                  offsetTop={-10}
+                >
+                  <div>
+                    <img src='/img/pin.png' alt='currentPosition' style={{ width: '20px' }} />
+                  </div>
+                </Marker>
+              ) : null}
+            </ReactMapGL>
+          ) : null}
+        </div>
+        <div className="view-title">
+          <h1>{this.state.title}</h1>
+        </div>
+        <div className="view-image">
+          <img src={this.state.scenePictureUrl} alt={this.state.title} />
+          <p>Uploaded by: {this.state.author} </p>
+        </div>
+
+        {this.state.owner ? (
+          <div className="view-links">
+            <Link className='view-links-component' to={'/location/' + this.state.id + '/update'}>Update</Link>
+            -
+            <Link className='view-links-component' to={'/location/' + this.state.id + '/delete'}>Delete</Link>
+          </div>
+        ) : null}
+      </div>
     );
   }
-
-
 }
 
 
