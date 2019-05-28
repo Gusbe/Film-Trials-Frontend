b2a7bebbee545befa2bcb1a19a7d1a22ed1ccc9e
gusbe <gusbertolo@gmail.com>
2019-05-29 00:38:43 +0200

    Added the list search in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,6 +10,9 @@ class Navbar extends Component {
           <Link className="link-nav" to='/'><img src="/img/map-icon.svg" alt='Map Search'/></Link>
         </div>
         <div>
+          <Link className="link-nav" to='/search'><img src="/img/map-icon.svg" alt='Search List'/></Link>
+        </div>
+        <div>
           <Link className="link-nav" to='/location/add'><img src="/img/map-add.svg" alt='Map Add'/></Link>
         </div>
       </div>
